e46f2c337b8224227cd875db0772e631044ddcc4
Dan Putnam <daniel.a.putnam@gmail.com>
2018-04-29 22:30:10 -0500

    basic structure add treeloitem component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
 import "./index.css";
 
+const TrelloItem = ({item}) => {
+  return (
+    <div>item 1</div>
+  );
+};
+
 const TrelloCard = () => (
     <div className="trello-card">
-      Card Here
+      Card Title
+      Items
+      <TrelloItem />
+      Add Card...
+
       {/* {files.map(file => (
         <FileListItem key={file.id} file={file}/>
       ))} */}
